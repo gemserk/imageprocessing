721ebb4d02674b9cd27f8367417f9d4e656ab33b
Ruben Garat <ruben.garat@gemserk.com>
2012-07-31 14:25:17 -0300

    added deploy options to pom, extracted execute method to be able to invoke as a library using the options object for configuration

--- src/main/java/com/gemserk/utils/imageprocessing/ColorBleedingCLI.java
+++ src/main/java/com/gemserk/utils/imageprocessing/ColorBleedingCLI.java
@@ -27,22 +27,22 @@ public class ColorBleedingCLI {
 		List<String> parameters = new ArrayList<String>();
 
 		@Parameter(names = "-dir", description = "Directory containing the images", required = true)
-		String dir;
+		public String dir;
 
 		@Parameter(names = "-overwrite", description = "Overwrite Source Images")
-		boolean overwrite = false;
+		public boolean overwrite = false;
 
 		@Parameter(names = "-debug", description = "Writes debug images without alpha")
-		boolean debug = false;
+		public boolean debug = false;
 
 		@Parameter(names = "-outdir", description = "Directory to use for the output (copy if not overriding and debug image if debug enabled)")
-		String outdir = null;
+		public String outdir = null;
 
 		@Parameter(names = "-maxiterations", description = "Max iterations to run the algorithm")
-		int maxiterations = Integer.MAX_VALUE;
+		public int maxiterations = Integer.MAX_VALUE;
 		
 		@Parameter(names = "-numthreads", description = "Number of threads to use")
-		int numthreads = 8;
+		public int numthreads = 8;
 
 	}
 
@@ -60,90 +60,7 @@ public class ColorBleedingCLI {
 			
 			jCommander.parse(args);
 
-			String dirPath = options.dir;
-			logger.info("Path: " + dirPath);
-			logger.info("Overwrite: " + options.overwrite);
-
-			final File dir = new File(dirPath);
-
-			if (!isDirectoryValid("input", dir))
-				return;
-
-			final File outDir = options.outdir == null ? dir : new File(options.outdir);
-
-			if (!isDirectoryValid("output", outDir))
-				return;
-
-			final ArrayList<File> images = new ArrayList<File>();
-
-			FileTraversal fileTraversal = new FileTraversal() {
-				@Override
-				public void onFile(File f) {
-					String name = f.getName();
-					if (name.endsWith(".png"))
-						images.add(f);
-				}
-			};
-
-			fileTraversal.traverse(dir);
-
-			ExecutorService executorService = Executors.newFixedThreadPool(options.numthreads);
-
-			long globalIniTime = System.currentTimeMillis();
-
-			for (int i = 0; i < images.size(); i++) {
-				final File imageFile = images.get(i);
-				final int index = i;
-				executorService.execute(new Runnable() {
-
-					@Override
-					public void run() {
-						try {
-							ColorBleedingEffect colorBleedingEffect = new ColorBleedingEffect();
-							long iniTime = System.currentTimeMillis();
-
-							logger.info("{} - Processing image {}", index, imageFile);
-							BufferedImage image = ImageIO.read(imageFile);
-							BufferedImage processedImage = colorBleedingEffect.processImage(image, options.maxiterations);
-							File destinationFile = imageFile;
-							File destinationFolder = destinationFolder(dir, imageFile, outDir);
-							if (dir != outDir)
-								destinationFolder.mkdirs();
-							if (!options.overwrite) {
-								destinationFile = new File(destinationFolder, imageFile.getName() + ".bleed");
-							}
-							ImageIO.write(processedImage, "png", destinationFile);
-
-							if (options.debug) {
-								int width = image.getWidth();
-								int height = image.getHeight();
-
-								BufferedImage processedImageOpaque = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-								int[] rgba = processedImage.getRGB(0, 0, width, height, null, 0, width);
-								processedImageOpaque.setRGB(0, 0, width, height, rgba, 0, width);
-
-								ImageIO.write(processedImageOpaque, "png", new File(destinationFolder, imageFile.getName() + ".debug"));
-							}
-
-							long now = System.currentTimeMillis();
-							float time = (now - iniTime) / 1000f;
-							logger.debug("{} - Time to process {} seconds", index, time);
-						} catch (IOException e) {
-							logger.error("{} - Error while processing images", index, e);
-						} catch (Exception e) {
-							logger.error("{} - Error while processing images", index, e);
-						}
-					}
-				});
-			}
-
-			executorService.shutdown();
-			executorService.awaitTermination(10, TimeUnit.HOURS);
-
-			long globalNow = System.currentTimeMillis();
-			float globalTime = (globalNow - globalIniTime) / 1000f;
-			logger.info("Time to process {} images - {} seconds", images.size(), globalTime);
+			execute(options);
 
 		} catch (ParameterException e) {
 			logger.error(e.getMessage());
@@ -157,6 +74,93 @@ public class ColorBleedingCLI {
 			e.printStackTrace();
 		}
 
+	}
+
+	public static void execute(final Options options) throws IOException, InterruptedException {
+		String dirPath = options.dir;
+		logger.info("Path: " + dirPath);
+		logger.info("Overwrite: " + options.overwrite);
+
+		final File dir = new File(dirPath);
+
+		if (!isDirectoryValid("input", dir))
+			return;
+
+		final File outDir = options.outdir == null ? dir : new File(options.outdir);
+
+		if (!isDirectoryValid("output", outDir))
+			return;
+
+		final ArrayList<File> images = new ArrayList<File>();
+
+		FileTraversal fileTraversal = new FileTraversal() {
+			@Override
+			public void onFile(File f) {
+				String name = f.getName();
+				if (name.endsWith(".png"))
+					images.add(f);
+			}
+		};
+
+		fileTraversal.traverse(dir);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(options.numthreads);
+
+		long globalIniTime = System.currentTimeMillis();
+
+		for (int i = 0; i < images.size(); i++) {
+			final File imageFile = images.get(i);
+			final int index = i;
+			executorService.execute(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						ColorBleedingEffect colorBleedingEffect = new ColorBleedingEffect();
+						long iniTime = System.currentTimeMillis();
+
+						logger.info("{} - Processing image {}", index, imageFile);
+						BufferedImage image = ImageIO.read(imageFile);
+						BufferedImage processedImage = colorBleedingEffect.processImage(image, options.maxiterations);
+						File destinationFile = imageFile;
+						File destinationFolder = destinationFolder(dir, imageFile, outDir);
+						if (dir != outDir)
+							destinationFolder.mkdirs();
+						if (!options.overwrite) {
+							destinationFile = new File(destinationFolder, imageFile.getName() + ".bleed");
+						}
+						ImageIO.write(processedImage, "png", destinationFile);
+
+						if (options.debug) {
+							int width = image.getWidth();
+							int height = image.getHeight();
+
+							BufferedImage processedImageOpaque = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+							int[] rgba = processedImage.getRGB(0, 0, width, height, null, 0, width);
+							processedImageOpaque.setRGB(0, 0, width, height, rgba, 0, width);
+
+							ImageIO.write(processedImageOpaque, "png", new File(destinationFolder, imageFile.getName() + ".debug"));
+						}
+
+						long now = System.currentTimeMillis();
+						float time = (now - iniTime) / 1000f;
+						logger.debug("{} - Time to process {} seconds", index, time);
+					} catch (IOException e) {
+						logger.error("{} - Error while processing images", index, e);
+					} catch (Exception e) {
+						logger.error("{} - Error while processing images", index, e);
+					}
+				}
+			});
+		}
+
+		executorService.shutdown();
+		executorService.awaitTermination(10, TimeUnit.HOURS);
+
+		long globalNow = System.currentTimeMillis();
+		float globalTime = (globalNow - globalIniTime) / 1000f;
+		logger.info("Time to process {} images - {} seconds", images.size(), globalTime);
 	}
 
 	private static File destinationFolder(File originalDir, File originalFile, File destinationRoot) {
